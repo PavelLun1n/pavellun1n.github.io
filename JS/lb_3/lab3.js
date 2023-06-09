224154b15f0513599b2055aeaf5ba512a1cb0253
PavelLun1n <106957514+PavelLun1n@users.noreply.github.com>
2023-06-09 11:11:33 +0300

    lab3.js

--- JS/lb_3/lab3.js
+++ JS/lb_3/lab3.js
@@ -1,44 +1,59 @@
 "use strict";
 
-// возвращает n-е число Фибоначчи
-import {fib}  from '../lb_2/fib.js';
+import {
+    fib
+} from './lab_2.js';
 
-//возвращает дробную часть числа num
+/**
+ * Возвращает дробную часть числа num.
+ * @param {num} num число, от которого нужно найти дробную часть.
+ * @return {num} дробная часть числа.
+ */
 export function getDecimal(num) {
-  let w = Math.abs(num - Math.floor(num));
-  return parseFloat(w.toFixed(2));
+    if (Math.trunc(num) == num) return 0;
+    else if (num < 0) return num - (-Math.ceil(-num));
+    if (num > 1) return +(num - Math.trunc(num)).toFixed(2);
 }
 
-
-//возвращает строку str с заглавным первым символом
+/**
+ * Возвращает строку str с заглавным первым символом.
+ * @param {str} str строка, которую нужно преобразовать.
+ * @return {str} str с заглавным первым символом.
+ */
 export function ucFirst(str) {
-  if (!str) {
-    return str;
-  } else {
-    return str[0].toUpperCase() + str.slice(1);
-  }
+    if (!str) return str;
+    else {
+        let newstr = str[0].toUpperCase() + str.slice(1);
+        return newstr;
+    }
 }
 
-
-//возвращает true, если строка str содержит 'viagra' или 'XXX', иначе false
+/**
+ * Возвращает true, если строка str содержит 'viagra' или 'XXX', а иначе false.
+ * @param {str} str проверяемая строка.
+ * @return {number} true, если строка str содержит 'viagra' или 'XXX', а иначе false.
+ */
 export function checkSpam(str) {
-  let lowerStr = str.toLowerCase();
-  
-  return lowerStr.includes('viagra') || lowerStr.includes('xxx');
+    let newstr = str.toLowerCase();
+    return newstr.includes('viagra') || newstr.includes('xxx');
 }
 
-
-// проверяет длину строки str, если она превосходит maxlength заменяет конец строки на "…"
+/**
+ * Проверяет длину строки str.
+ * @param {str} str Проверяемая строка.
+ * @param {number} maxlength максимальная длина строки.
+ * @return {str} str, усеченная строка.
+ */
 export function truncate(str, maxlength) {
-  if (str.length > maxlength) {
-    return str.slice(0, maxlength - 1) + '…';
-  } else {
-    return str;
-  }
+    if (str.length < maxlength) return str;
+    else return str.slice(0, maxlength - 1) + '…';
 }
 
-
-//преобразует строку вида 'var-test-text' в 'varTestText'
+/**
+ * Дефисы в строке удаляются, а все слова после них получают заглавную букву.
+ * @param {str} str Проверяемая строка.
+ * @return {str} strwew, преобразованная строка.
+ */
 export function camelize(str) {
   let words = str.split('-');
   
@@ -48,30 +63,34 @@ export function camelize(str) {
   
   return words.join('');
 }
-
-
-
-
-
-//для натурального n возвращает массив, заполненный числами Фибоначчи до n-го (не включая его)
-export function fibs(n) {
-  const arr = [];
-  for (let i = 0; i < n; i++) {
-    arr.push(fib(i));
-  }
-  return arr;
+/**
+ * Возвращает массив, заполненный числами Фибоначчи до n-го числа (не включая его).
+ * @param {number} n натуральноее число, количество чисел Фибоначчи.
+ * @return {Array} arr, массив с числами Фибоначчи.
+ */
+export function  fibs(n){
+    let arr = [];
+    for (let i = 0; i < n; i++) arr.push(fib(i));
+    return arr;
 }
 
-
-//принимает неупорядоченный массив чисел arr и возвращает массив из тех же элементов
+/**
+ * Возвращает массив из элементов, отсортированный по убыванию.
+ * @param {Arr} arr массив элементов.
+ * @return {Arr} arr, массив, отсортированный по убыванию.
+ */
 export function arrReverseSorted(arr) {
-  const sortedArr = arr.slice().sort((a, b) => b - a);
-  
-  return sortedArr;
+  const sorted = arr.slice().sort((a, b) => b - a);
+  return sorted;
 }
 
-
-//вернёт массив уникальных, не повторяющихся значений массива arr
+/**
+ * Возвращает массив уникальных, не повторяющихся значений.
+ * @param {Arr} arr массив элементов.
+ * @return {Arr} arr, массив с уникальными значениями.
+ */
 export function unique(arr) {
-  return Array.from(new Set(arr));
+    let set = new Set();
+    for (let i in arr) set.add(arr[i]);
+    return Array.from(set);
 }
