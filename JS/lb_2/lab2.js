7e25965ef356a89c4a5ac23c1206e314762fe971
PavelLun1n <106957514+PavelLun1n@users.noreply.github.com>
2023-06-09 11:12:51 +0300

    lab2.js

--- JS/lb_2/lab2.js
+++ JS/lb_2/lab2.js
@@ -1,64 +1,69 @@
-//функция возвращает x в степени n (n - целое число).
-function pow(x, n)
-{
-    return x**n; //возвращаем х в степени n
-}
-// функция вычисляет сумму чисел от 1 до n включительно.
-function sumTo(n)
-{
-    return (n*(n+1))/2  //формула суммы арифметической прогрессии
+"use strict";
 
+function pow(x,n){
+  if (n == 0){
+    return 1;
+  }
+  else if (n < 0){
+    return 1 / pow(x,-n);
+  }
+  else {
+    return x * pow(x,n - 1);
+  }
 }
-//функция возвращает факториал числа n
-function factorial(n)
-{
-    if (n<=0)
-    {
-        return 1n; //возвращаем 1 для чисел, меньших 0
-    }
-    else{
-        return ( BigInt (n) *  BigInt(factorial(n-1))); //использование рекурсии 
-    }
+
+function sumTo(n){
+  if (n == 1){
+    return 1;
+  }
+  else {
+    return n + sumTo(n - 1);
+  }
 }
-//функция возвращает n-е число Фибоначчи
-function fib(n){
-    a = BigInt(1); // первое число Фиббоначи
-    b=BigInt(1); //второе число Фиббоначи
-    if (n== BigInt(0))
-    {
-        return BigInt(0); //возвращаем нуль при n=0
-    }
-    else
-    {
-        //алгоритм нахождения чисел Фиббоначи (базовый)
-        for (i = BigInt(3); i<=n; i++) 
-        {
-            c = a+b;
-            a=b;
-            b=c;
+
+function factorial(n){
+  if (n == 0){
+    return 1n;
+  }
+  else{
+    return BigInt(n) * factorial(n - 1);
+  }
+}
+
+export function fib(n){
+  let x = 0n;
+  let y = 1n;
+  if (n == 0){
+    return x;
+  }
+  else if (n == 1){
+    return y
+  }
+  else{
+    for (let j = 2; j <= n; j++) {
+            let c = x + y;
+            x = y;
+            y = c;
         }
-        return b;
+
+        return y;
+  }
+}
+
+function compare(x){
+  return function (y){
+    if (x == y){
+      return null;
     }
-}
-//функция принимает целочисленное значение x и возвращает 
-//анонимную функцию. Анонимная функция возвращает 
-//результат сравнения значений y и x
-function compare(x)
-{
-return function(y)
-{
-    if (y>x) return true;
-    else if (y<x) return false;
-    else if (y==x) return null;
-}
-}
-//функция возвращает сумму всех своих аргументов.
-function sum()
-{
-    var result=0; //сумма
-    for (var i=0; i<arguments.length; i++)
-    {
-        result+=arguments[i] //складываем элементы на соответствующих местах
+    if (x > y){
+      return false
     }
-    return result;
+    if (x < y){
+      return true
+    }
+  }
+}
+
+function sum(...args) {
+    return args.reduce((acc, curr) => acc + curr, 0);
 }
